2bf8eb64c55311dc159c661d52fae02448820868
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-04-13 15:01:00 +0900

    refactor: Add original message when error or warning
    re #3

--- tests/.stylelintrc.js
+++ tests/.stylelintrc.js
@@ -28,12 +28,25 @@ module.exports = {
     "declaration-block-no-shorthand-property-overrides": true,
     indentation: 4,
 
-    // Thou shalt not nest
-    "selector-max-specificity": "0,1,0",
-    // Thou shalt not use ID selectors
-    "selector-max-id": 0,
-    // Thou shalt not write vendor prefixes in the authoring style sheets
-    "property-no-vendor-prefix": true,
+    "selector-max-specificity": [
+      "0,2,0",
+      {
+        message: "Thou shalt not nest"
+      }
+    ],
+    "selector-max-id": [
+      0,
+      {
+        message: "Thou shalt not use ID selectors"
+      }
+    ],
+    "property-no-vendor-prefix": [
+      true,
+      {
+        message:
+          "Thou shalt not write vendor prefixes in the authoring style sheets"
+      }
+    ],
     // @WIP Thou shalt use variables for sizing, colours and z-index
     "scale-unlimited/declaration-strict-value": [
       ["/color/", "fill", "stroke", "z-index"],
@@ -44,7 +57,13 @@ module.exports = {
         }
       }
     ],
-    // ECSS selectors
+    "property-blacklist": [
+      "max-width",
+      {
+        severity: "warning",
+        message: "Thou shalt always write rules mobile first"
+      }
+    ],
     "selector-class-pattern": [
       "^[a-z][a-z0-9]*(-[A-Z][a-zA-Z0-9]*(_[A-Z][a-zA-Z0-9]*(-[a-z][a-z0-9]*)?)?)?$",
       {
